965a1f648470651cf43f343bbfa9b4c22c7ae9d2
Frie <fr1e@pm.me>
2020-12-07 16:57:23 +0100

    fake data generator

---
+++ inst/generate_fake_applications.R
@@ -0,0 +1,49 @@
+# generate fake data for applications
+library(charlatan)
+library(dplyr)
+set.seed(123)
+fraud <- fraudster()
+int_prov <- InternetProvider
+
+survey_id <- 287163371
+appl_en <- survey_id %>% 
+  surveymonkey::fetch_survey_obj() %>%
+  surveymonkey::parse_survey() 
+n <- nrow(appl_en)
+appl_en$survey_id <- 2123912391
+appl_en$respondent_id <- fraud$integer(n)
+dt_prov <- DateTimeProvider$new()
+appl_en$date_created <- purrr::map_chr(1:n, ~dt_prov$date_time() %>% as.character())
+appl_en$date_modified <- purrr::map_chr(1:n, ~dt_prov$date_time() %>% as.character())
+appl_en$`What is your first name?` <- fraud$name(n)
+
+# gender 
+appl_en$`What is your gender?<br><em>Note: we work according to the principle of gender equality. This is why this question is particularly important to us.</em> - My gender is (please specify):`[12] <- "agender"
+appl_en$`What is your gender?<br><em>Note: we work according to the principle of gender equality. This is why this question is particularly important to us.</em>`[12] <- NA
+
+# fake emails 
+appl_en$`Which email address can we use to contact you?` <-  purrr::map_chr(1:n, ~int_prov$new()$free_email())
+
+# fake motivation statements 
+text_prov <- charlatan::LoremProvider$new()
+appl_en$`Please describe here why you want to get involved in this project (max. 5 sentences)` <- purrr::map_chr(1:n, ~text_prov$text()) 
+appl_en$`Please describe here what skills you would bring to the project (max. 5 sentences).` <- purrr::map_chr(1:n, ~text_prov$text()) 
+
+# fake ips
+appl_en$ip_address <-  purrr::map_chr(1:n, ~int_prov$new()$ipv4())
+
+# rating questions
+base_prov <- BaseProvider$new()
+fake_rating <- function(x) base_prov$random_element(c("Advanced", "Beginner", "User", "Expert"))
+rating_cols <- stringr::str_subset(colnames(appl_en), "rate")
+
+for (col in rating_cols) {
+  appl_en[, col] <- purrr::map_chr(1:n, fake_rating)
+}
+
+# fake role
+fake_role <- function(x) base_prov$random_element(c("Team trainee", "Team member", "Team lead"))
+appl_en$`What role do you want to assume in the project?` <- purrr::map_chr(1:n, fake_role)
+
+appl_en %>% readr::write_rds("tests/testthat/test_data/surveymonkey/applications_fake_en.rds")
+appl_en %>% readr::write_csv("tests/testthat/test_data/surveymonkey/applications_fake_en.csv")
