4c4d6417b5567915446db7352187e7be27e2ff1d
Frie <fr1e@pm.me>
2020-12-08 14:06:34 +0100

    more comments to template, drop columns

--- inst/templates/download_applications.R
+++ inst/templates/download_applications.R
@@ -1,6 +1,7 @@
 library(projectutils)
 library(readr)
 library(glue)
+library(dplyr)
 # download applications
 PROJECT_ID <- "{{{project_id}}}"
 project_id_lower <- tolower(PROJECT_ID)
@@ -9,13 +10,21 @@ data_folder <- here::here() # change this if your data folder is somewhere else 
 project_folder <- fs::path(data_folder, id_path(PROJECT_ID))
 appl <- load_applications(PROJECT_ID)
 
+# mapping of ids to emails / names -> only for project coordinator / local
 appl %>% 
-  anonymize_applications() %>% 
-  write_csv(glue("{project_folder}/{project_id_lower}_applications_anonymized.csv"))
+  select(applicant_id, email, first_name) %>% 
+  write_csv(glue::glue("{project_folder}/{project_id_lower}_mapping.csv"))
 
+# motivation questions for team selection committee
 appl %>% 
   extract_motivation_questions() %>% 
   write_lines(glue("{project_folder}/{project_id_lower}_applications_motivation.md"))
+
+# anonymized version for team selection committee
+appl %>% 
+  anonymize_applications() %>% 
+  select(-starts_with("motivation_")) %>% # drop long-text variables bc they blow up the csv
+  write_csv(glue("{project_folder}/{project_id_lower}_applications_anonymized.csv"))
 
 # quick analytics
 table(appl$gender)
