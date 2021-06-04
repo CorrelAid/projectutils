88bb36ea5a26cff0dd0fac490e8dc00eafdd13ee
Frie <fr1e@pm.me>
2021-06-04 10:42:22 +0200

    stricter checking of project id, fix tests

--- R/utils.R
+++ R/utils.R
@@ -39,4 +39,15 @@ assert_project_id = function(value) {
     usethis::ui_stop("project_id needs to be a character vector of length 1.")
   }
   if (!stringr::str_detect(value, "^\\d{4}\\-\\d{2}\\-[:upper:]{3,3}$")) usethis::ui_stop("Invalid project id. It needs to be conform to the following format: YYYY-mm-ABB where ABB is a three-letter abbreviation of the partner organization.")
+
+  year  <- as.numeric(stringr::str_sub(value, 1, 4))
+  month  <- stringr::str_sub(value, 6,7)
+
+  if(year < 2015 | year > 2030) {
+    usethis::ui_stop("Invalid year in project id. Only years between 2015 and 2030 are valid.")
+  }
+  months  <- c(paste0('0', c(0:9)), 10, 11, 12)
+  if (!month %in% months) {
+    usethis::ui_stop("Invalid month in project id.")
+  }
 }
