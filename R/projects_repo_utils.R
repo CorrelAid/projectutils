94cda3b40951af971945235fce16980c732291e6
Frie <fr1e@pm.me>
2021-06-18 11:57:53 +0200

    fix check warnings

--- R/projects_repo_utils.R
+++ R/projects_repo_utils.R
@@ -4,6 +4,7 @@
 #' @param github_board_id character. internal id of the Project in the GitHub API. Get via the API. Defaults to the ID for the CorrelAid projects board.
 #' @return character. status / column of the project in the kanban board
 #' @export
+#' @importFrom rlang .data
 get_project_status <- function(gh_issue_num, github_board_id = '3771798') {
     columns <- gh::gh("/projects/{project_id}/columns",
         project_id = github_board_id,
@@ -16,15 +17,15 @@ get_project_status <- function(gh_issue_num, github_board_id = '3771798') {
             issue_urls <- gh::gh(column$cards_url, .accept = "application/vnd.github.inertia-preview+json") %>%
                 purrr::map_chr("content_url", .default = NA)
             tibble::tibble(name = column$name, issue_url = issue_urls) %>%
-                dplyr::mutate(issue_num = stringr::str_replace_all(issue_url, ".+?/(\\d{1,})$", "\\1"))
+                dplyr::mutate(issue_num = stringr::str_replace_all(.data$issue_url, ".+?/(\\d{1,})$", "\\1"))
         })
     if (!gh_issue_num %in% cards$issue_num) {
         usethis::ui_warn(glue::glue("{gh_issue_num} is not part of the board. Returning NA.\n"))
         return(NA)
     }
     cards %>%
-        filter(issue_num == gh_issue_num) %>%
-        pull(name)
+        dplyr::filter(.data$issue_num == gh_issue_num) %>%
+        dplyr::pull(.data$name)
 }
 
 #' get_gh_issue_data
@@ -32,6 +33,7 @@ get_project_status <- function(gh_issue_num, github_board_id = '3771798') {
 #' @param project_id character. id of the project, in the form YYYY-mm-ABB where ABB is any three-character, uppercase abbreviation
 #' @return list. list with title, number, body and labels sub lists
 #' @export
+#' @importFrom rlang .data
 get_gh_issue_data <- function(project_id) {
     gh_issues <- gh::gh("GET /repos/{owner}/{repo}/issues", repo = "projects", owner = "correlaid", state = "all", .limit = Inf,
     .accept = 'application/vnd.github.starfox-preview')
@@ -52,18 +54,18 @@ get_gh_issue_data <- function(project_id) {
 
     # tags and local chapter
     labels_df <- gh_issue$labels %>%
-        map_chr("name") %>%
+        purrr::map_chr("name") %>%
         tibble::tibble(name = .) %>%
-        tidyr::separate(name, into = c("category", "value"), sep = ":", fill = "right")
+        tidyr::separate(.data$name, into = c("category", "value"), sep = ":", fill = "right")
     # local chapters based on tags
     lcs <- labels_df %>%
-        filter(category == "lc") %>%
-        pull(value)
+        dplyr::filter(.data$category == "lc") %>%
+        dplyr::pull(.data$value)
 
     return(
         list(
             name = name, 
-            tags = labels_df %>% filter(category != 'lc'), 
+            tags = labels_df %>% dplyr::filter(.data$category != 'lc'), 
             lc = lcs,
             num_gh_issue = gh_issue$number,
             text = gh_issue$body, 
@@ -73,12 +75,16 @@ get_gh_issue_data <- function(project_id) {
     )
 }
 
-#' extract codimd pad links from text
+#' extract codimd pad links from text 
+#' @param txt character. text of the github issue.
+#' @return character. vector of pad links
 extract_pad_links <- function(txt) {
     unlist(stringr::str_extract_all(txt, "https://pad.correlaid.org/.+"))
 }
 
 #' extract urls
+#' @param txt character. text of the github issue.
+#' @return character. vector of urls
 extract_urls <- function(txt) {
     unlist(stringr::str_extract_all(txt, "(http(s)?:\\/\\/.)?(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)"))
 }
